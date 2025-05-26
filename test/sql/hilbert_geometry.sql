99375477ee66780c7dbad5d44444e3f1fc784e4e
alamminsalo <0dosages-remits@icloud.com>
2025-05-26 22:42:57 +0300

    feat: add test with osm data

--- test/sql/hilbert_geometry.sql
+++ test/sql/hilbert_geometry.sql
@@ -1,5 +1,10 @@
 load 'build/debug/extension/hilbert_geometry/hilbert_geometry.duckdb_extension';
 load spatial;
 
--- Outputs POINT(1 1) geometry
-select st_aswkb('POINT(1 1)').hg_encode().hg_decode().st_geomfromwkb();
+-- load osm data into table
+create table data as select geometry from 'test/sql/files/*.parquet';
+select count(*) from data;
+
+-- copy parquet with geometries only for comparison
+copy data to 'test/sql/geom.parquet';
+copy (select geometry.st_aswkb().hg_encode() as hg from data) to 'test/sql/geom_hg.parquet';
