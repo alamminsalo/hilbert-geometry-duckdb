bfc2cf3fb7e81a6f4dfae3ce66ff3d782afbc1aa
alamminsalo <0dosages-remits@icloud.com>
2025-05-27 08:38:38 +0300

    chore: update readme

--- README.md
+++ README.md
@@ -22,6 +22,21 @@ SELECT hg_decode('\x00\x1C\xC0\x9C7@\x1E\x1D?'::blob).st_geomfromwkb();
 -- POINT (1 1)
 ```
 
+## Examples
+
+Running the [osm.sql](test/sql/osm.sql) extracts a regional OSM dataset and writes parquet files with both WKB and HWKB encoded geometries.
+
+```
+# Execute test script
+duckdb -unsigned -bail < test/sql/osm.sql
+
+# Check output file sizes
+du -sh test/sql/*.parquet
+
+# 1.4M    test/sql/osm_hwkb.parquet
+# 3.6M    test/sql/osm_wkb.parquet
+```
+
 ## Building
 
 See instructions at [duckdb/extension-template-rs](https://github.com/duckdb/extension-template-rs)
--- test/sql/osm.sql
+++ test/sql/osm.sql
@@ -1,22 +1,23 @@
 --
-LOAD 'build/debug/extension/hilbert_geometry/hilbert_geometry.duckdb_extension';
+LOAD 'build/release/hilbert_geometry.duckdb_extension';
 LOAD spatial;
 
 SET s3_region='us-west-2';
 
--- Read overturemaps OSM data from Osaka, Japan
+.print 'Reading OSM data from Osaka, Japan...'
 CREATE TABLE data AS
     SELECT
-       geometry   -- DuckDB v.1.1.0 will autoload this as a `geometry` type
-    FROM read_parquet('s3://overturemaps-us-west-2/release/2025-05-21.0/**/*.parquet', filename=true, hive_partitioning=1)
+       geometry
+    FROM read_parquet('s3://overturemaps-us-west-2/release/2025-05-21.0/theme=*/type=*/*.parquet', hive_partitioning=1)
     WHERE TRUE
-      AND bbox.xmin > 135.178528
-      AND bbox.ymin > 34.438628
-      AND bbox.xmax < 135.796509
-      AND bbox.ymax < 34.883678
+    AND theme IN ('building', 'places', 'transportation')
+    AND bbox.xmin > 135.42
+    AND bbox.ymin > 34.62
+    AND bbox.xmax < 135.52
+    AND bbox.ymax < 34.72
 ;
 
 
--- copy parquet with geometries only for comparison
+.print 'Creating parquet files...'
 copy data to 'test/sql/osm_wkb.parquet';
 copy (select geometry.st_aswkb().hg_encode() as hg from data) to 'test/sql/osm_hwkb.parquet';
